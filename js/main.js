3b7fc4d03c6b707962287e0f73557e995d5dce79
Adrian Obelmejias <obeladrian@gmail.com>
2013-06-18 11:55:33 -0430

    commit

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ for (var i = 0, j = payTypes.length; i < j; i++) {
 
 var mongoLabs = function() {
 
-	function insertPayment(idFactura, publicKey, status) {
+	this.insertPayment = function(idFactura, publicKey, status) {
 		var passKey = window.btoa(idFactura).substring(0,4);
 
 		var payment = {}
@@ -25,10 +25,15 @@ var mongoLabs = function() {
 		payment.creationDate	= new Date();
 		payment.passKey 		= passKey;
 
-		
-
+    $.ajax({ 
+      url: "https://api.mongolab.com/api/1/databases/cinesunidos/collections/payments?apiKey=504557bce4b072c137d81770",
+      data: JSON.stringify(payment),
+      type: "POST",
+      contentType: "application/json" 
+    });
+	
 		return passKey;
 	}
 
 	return this
-}
+}();
